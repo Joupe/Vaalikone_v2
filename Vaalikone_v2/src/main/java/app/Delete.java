2d174aa9bfc1033b83b2aae004231b2ca9178fbc
Joupe <joelkailanto@gmail.com>
2022-04-06 14:05:08 +0300

    Update Delete.java

--- Vaalikone_v2/src/main/java/app/Delete.java
+++ Vaalikone_v2/src/main/java/app/Delete.java
@@ -23,7 +23,7 @@ public class Delete extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		String id = request.getParameter("question_id");
+		String id = request.getParameter("id");
 		ArrayList<Question> list = null;
 		if (dao.getConnection()) {
 			list = dao.deleteQuestion(id);
